ee12e022691f67288275ad779a3eaf44f1ba8588
Mark Nuiver <marknuiver@hoppinger.com>
2018-07-05 13:14:27 +0200

    Random string generator / WIP

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,25 @@ import './App.css';
 
 import logo from './logo.svg';
 
+let randomnr:string = ''
+
+const makeid = ():string => {
+  let text = "";
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < 5; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+const createRandomString = ():string => {
+  const nwstring = setTimeout(() => {
+      randomnr = makeid()
+      createRandomString()
+      return randomnr;
+  }, 5000);
+}
 
 class App extends React.Component {
   public render() {
@@ -10,6 +29,7 @@ class App extends React.Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <h1>{createRandomString()}</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
